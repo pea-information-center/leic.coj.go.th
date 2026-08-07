00842b73fbe031561311da1c8a367470e49bee1b
สำนักปลัดนายกรัฐมนตรี สำนักพระราชวังดุสิต DGA/TDGA <royal_thai_police_loei@outlook.co.th>
2026-08-08 01:59:14 +0700

    Create Map.js

---
+++ Map.js
@@ -0,0 +1,182 @@
+chp-incidents: 13 incidents added, 17 incidents removed, 171 incidents changed
+13 new incidents:
+  CHP Incident 171114SA01269
+    https://www.google.com/maps/search/-121.464525,38.510303
+    Nov 14 2017  4:58PM  FIRE-Report of Fire  Martin Luther King Jr Blvd / 47th Ave 
+   Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114GGFSP00185
+    https://www.google.com/maps/search/-122.125001,37.892862
+    Nov 14 2017  4:57PM  1179-Trfc Collision-1141 Enrt  Sr24 E / Oak Hill Rd Ofr 
+  Nov 14 2017  4:50PM [6] [6] 1039 1141[Shared]Nov 14 2017  4:48PM [1] [1] MAR 15 ACUR RDX VS BLK FORD 4D SUV[Shared] Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114GG02348
+    https://www.google.com/maps/search/-122.196026,38.171987
+    Nov 14 2017  4:57PM  1125-Traffic Hazard  I80 E / American Canyon Rd Onr 
+  Nov 14 2017  4:58PM [1] BOX // #3 Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114GG02346
+    https://www.google.com/maps/search/-122.392204,37.750228
+    Nov 14 2017  4:56PM  1182-Trfc Collision-No Inj  I280 S / Cesar Chavez St W Onr 
+  Nov 14 2017  4:57PM [3] ON RHS Nov 14 2017  4:57PM [2] WHI NISS TITAN VS SIL TOYT COA  Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114VT00245
+    https://www.google.com/maps/search/-119.30568,34.281691
+    Nov 14 2017  4:57PM  1125-Traffic Hazard  Sr33 S / Main St 
+  Nov 14 2017  4:57PM [1] TABLE LEGS IN LNS Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114MY00290
+    https://www.google.com/maps/search/-121.611938,36.644593
+    Nov 14 2017  4:56PM  1182-Trfc Collision-No Inj  Harris Rd / Abbott St 
+  Nov 14 2017  4:58PM [2] TRLR O/TURNEDNov 14 2017  4:58PM [1] SOLO SEMI W/ A FLTBED TRLR Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114LA01977
+    https://www.google.com/maps/search/-118.13225,34.151987
+    Nov 14 2017  4:59PM  1183-Trfc Collision-Unkn Inj  I210 W / N Lake Ave 
+  Nov 14 2017  5:00PM [1] 4 VEHS INVL'D, ON RS AREA Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114LA01625
+    https://www.google.com/maps/search/-118.41378,34.156833
+    Nov 14 2017  3:43PM  1125-Traffic Hazard  US101 W / COLDWATER CANYON BLVD 
+  Nov 14 2017  4:46PM [22] [Notification] [CHP]-PER 3A , SHERMAN OAKS 1022'D DUE TO PTY BEING BELLIGERENT / 3A SENDING OUT ANOTHER PROVIDER UNK ETA / SO#50216 [Shared]Nov 14 2017  3:55PM [21] RECALLED 3A  CORRECTED ETA 25 MIN [Shared]Nov 14 2017  3:51PM [20] [Notification] [CHP]-1039 3A // SO# 48784 SHERMAN OAKS CLUB SERVICE 818-995-1866 ETA 75 MINS OL [Shared]Nov 14 2017  3:48PM [19] 3A REQ FOR A SIL MERZ CP ; RS WB 101 JEO WOODMAN  [Shared]Nov 14 2017  3:47PM [18] [Notification] [CHP]-[17] FSP CAN 1022 ; GOING TO ROLL 3A  [Shared] [Shared]Nov 14 2017  3:47PM [17] FSP CAN 1022 ; GOING TO ROLL 3A  [Shared]Nov 14 2017  3:46PM [16] [Notification] [CHP]-[15] VEH PUSHED TO THE RS ; LANES CLEAR ; PLS CONT FSP TO THE RS  [Shared] [Shared]Nov 14 2017  3:46PM [15] VEH PUSHED TO THE RS ; LANES CLEAR ; PLS CONT FSP TO THE RS  [Shared]Nov 14 2017  3:45PM [13] [Notification] [CHP]-[12] A56-P1 IS 10-97 ; BETTER 1020 IS WB WO COLDWATER CANYON ; BLKING THE 2 LANE  [Shared] [Shared]Nov 14 2017  3:45PM [12] A56-P1 IS 10-97 ; BETTER 1020 IS WB WO COLDWATER CANYON ; BLKING THE 2 LANE  [Shared]Nov 14 2017  3:43PM [1] 2 LANE ; SIL MERZ Nov 14 2017  3:31PM [2] [Appended, 15:44:41] [1] IN THE #2 LN STALLED ELDERLY XRAY POSS BLU VEHICLE   Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114FR00373
+    https://www.google.com/maps/search/-119.742792,36.701571
+    Nov 14 2017  4:58PM  1183-Trfc Collision-Unkn Inj  4667 E Date Ave 
+  Nov 14 2017  5:00PM [3] MALE OOF VEHNov 14 2017  4:59PM [2] 1125Nov 14 2017  4:59PM [1] PK VS POLE Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114MR00307
+    https://www.google.com/maps/search/-120.498825,37.452106
+    Nov 14 2017  4:58PM  1183-Trfc Collision-Unkn Inj  11351 Sr59 
+  Nov 14 2017  4:58PM [4] 1039 RIGGS Nov 14 2017  4:58PM [2] OVERTURNED VEH  Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114BCFSP00101
+    https://www.google.com/maps/search/-117.141062,32.832328
+    Nov 14 2017  5:00PM  1125-Traffic Hazard  Sr163 S / Clairemont Mesa 
+  Nov 14 2017  5:00PM [1] TURNING FOR SIL NISS SUV IN THE GP Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114BCFSP00100
+    https://www.google.com/maps/search/-117.15576,32.735344
+    Nov 14 2017  4:58PM  1183-Trfc Collision-Unkn Inj  Sr163 N / Richmond St Ofr 
+  Nov 14 2017  4:58PM [2] [2] SDPD LT IS TURNING FOR A TC IN THE AREA, NFD[Shared] Information courtesy of 
+  Last updated: 11/14/2017 5:01pm 
+
+  CHP Incident 171114BC01039
+    https://www.google.com/maps/search/-117.15576,32.735344
+    Nov 14 2017  4:58PM  1183-Trfc Collision-Unkn Inj  Sr163 N / Richmond St Ofr 
+  Nov 14 2017  4:58PM [2] SDPD LT IS TURNING FOR A TC IN THE AREA, NFD Information courtesy of 
+  Last updated: 11/14/2017 5:01pm
+
+17 incidents removed:
+  CHP Incident 171114SAFSP00087
+    https://www.google.com/maps/search/-121.308186,38.578544
+    Nov 14 2017  4:41PM  1183-Trfc Collision-Unkn Inj  Us50 W / Mather Field Rd 
+  Nov 14 2017  4:41PM [1] RHS MAR VEH VS DRK VEH / ALL PTYS OO VEHS Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114GG02118
+    https://www.google.com/maps/search/-122.035633,37.560726
+    Nov 14 2017  3:53PM  1182-Trfc Collision-No Inj  I880 S / Decoto Rd E Onr 
+  Nov 14 2017  4:07PM [16] [FSP] has closed their incident []Nov 14 2017  4:03PM [15] B25-071  3 VEH 1182 [Shared]Nov 14 2017  4:03PM [14] B25-071  3 VEHS TO RHS [Shared]Nov 14 2017  3:58PM [10] 1039 3A #  38529 PRO STAR TOW  [Shared]Nov 14 2017  3:56PM [9] 1039 25-184 WILL BE ENRT WHEN CLR LOG GG01968 [Shared]Nov 14 2017  3:54PM [6] 1125 #2 REQ FSNov 14 2017  3:54PM [2] #2 Nov 14 2017  3:53PM [1] RP IN SIL HONDA CIV VS WHI TOYT COR / WHI HYUN Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114LA01965
+    https://www.google.com/maps/search/-118.48475,34.267678
+    Nov 14 2017  4:55PM  1125-Traffic Hazard  Sr118 E / Woodley Ave 
+  Nov 14 2017  4:56PM [1] CONE IN #2 LN  Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114LA01881
+    https://www.google.com/maps/search/-118.304663,33.938199
+    Nov 14 2017  4:31PM  1182-Trfc Collision-No Inj  W 108th St / S Denker Ave 
+  Nov 14 2017  4:36PM [1] 1461 W 108 TH ST BTWN NORMANDIE AND DENKER , 2 VEH TC  , BURG 16 HONDA CIV VS BRO UPS TRK BLOCKING - SAG Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114LA01704
+    https://www.google.com/maps/search/-118.413666,34.005972
+    Nov 14 2017  4:00PM  1179-Trfc Collision-1141 Enrt  I405 S / Washington Blvd 
+  Nov 14 2017  4:27PM [22] CORRECTION :   PER  S20   PLS ADV   ALL CITY   VEHS AT CENTER MEDIAN   ...... 10-39  ALL CITY COPIES VEHS AT CD Nov 14 2017  4:26PM [21] PER S20 PLS ADV  ALL CITY  VEHSNov 14 2017  4:12PM [19] [Notification] [CHP]-1039 ALL CITY 323-934-0404 ENRT W FB ETA 30 OL [Shared]Nov 14 2017  4:05PM [15] PLS ROLL 1 1185 FLT/BD Nov 14 2017  4:05PM [14] 79-S20 - ASC EXTENT OF INJURIES - NR 79-64 Nov 14 2017  4:02PM [6] 1039 CCFD////                             Nov 14 2017  4:01PM [10] [Appended, 16:03:51] [2] IN CDNov 14 2017  4:01PM [3] [Appended, 16:02:04] [1] RP INVOVLED -- IN A TOYT CAMERY --  BLOCKING HOV LNS Nov 14 2017  4:01PM [1] CIV MC DOWN, HOV LN BLOCKED///  Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114LA01641
+    https://www.google.com/maps/search/-118.029207,33.922749
+    Nov 14 2017  3:46PM  1179-Trfc Collision-1141 Enrt  Leffingwell Rd / Valley View Ave 
+  Nov 14 2017  4:08PM [5] 1039 LASO - N/E CORNER BY CWASHNov 14 2017  3:47PM [1] LASO AND FIRE 1097 Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114LA01250
+    https://www.google.com/maps/search/-117.990269,34.066347
+    Nov 14 2017  2:11PM  1182-Trfc Collision-No Inj  I10 E / Frazier St 
+  Nov 14 2017  2:54PM [12] 1039 HADDICKS // COPIES 1022 BOT [Shared]Nov 14 2017  2:51PM [11] TASK 81-104 COLA, PLS CANCEL HADDICKS -- PARTY HAS THEIR OWN TOW ENRT [Shared]Nov 14 2017  2:25PM [9] [FSP] has closed their incident [171114LAFSP00084]Nov 14 2017  2:16PM [6] [Notification] [CHP]-1039 HADDICKS BPP 323-692-3795 ETA 20 OL W FB [Shared]Nov 14 2017  2:13PM [3] TASK SO-203 COLA, PLS ROLL 1185 FB  [Shared] Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114FR00366
+    https://www.google.com/maps/search/-119.68962,36.841021
+    Nov 14 2017  4:49PM  1125-Traffic Hazard  Sr168 W / Fowler Ave Onr 
+   Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114FR00371
+    https://www.google.com/maps/search/-120.151282,36.690751
+    Nov 14 2017  4:40PM  1182-Trfc Collision-No Inj  S Butte Ave / W North Ave 
+  Nov 14 2017  4:57PM [27] 1348/65-41A SOLO VEH 1182, NEG 180S.. 1098 Nov 14 2017  4:56PM [26] 1347/65-41A ADV AC, BLK DOG IFO 2457 S BUTTE, JNO MUSCAT Nov 14 2017  4:55PM [25] 1329/65-41A RDWY OPEN Nov 14 2017  4:54PM [19] 1250/65-41A  6LEH114.. START 1185 Nov 14 2017  4:53PM [18] 1247/65-41A 1097.  IT IS 1125 Nov 14 2017  4:53PM [14] TOC/3RD CALLER/1233 HRS Nov 14 2017  4:49PM [9] VEH VS FENCE AND DOG.. DOG IS 1144 Nov 14 2017  4:49PM [7] TOC/2ND CALLER/1225 HRS Nov 14 2017  4:49PM [5] 1125 DEBRIS AND PEDSNov 14 2017  4:49PM [4] 1 RED VEH OVERTURNED VS POSS ANOTHER UNK TYPE VEH Nov 14 2017  4:48PM [3] 2 VEHS INVLD Nov 14 2017  4:48PM [1] TOC/1225 HRS  Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114FR00352
+    https://www.google.com/maps/search/-119.789394,36.855465
+    Nov 14 2017  4:36PM  1125-Traffic Hazard  Sr41 N / Friant Rd N Onr 
+  Nov 14 2017  4:36PM [2] DUMPING TRASH IN CDNov 14 2017  4:36PM [1] SIL OR TAN VAN IN CD Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114BC00986
+    https://www.google.com/maps/search/-117.016345,32.74194
+    Nov 14 2017  4:25PM  1183-Trfc Collision-Unkn Inj  Sr125 N / Sr94 
+  Nov 14 2017  4:50PM [16] 1039 3A SO# 51370  USA TOW  [Shared]Nov 14 2017  4:46PM [14] 1039 3A SO# 51191   USA TOW  [Shared]Nov 14 2017  4:31PM [12] PM ADV POSS NEEDS MEDICS , HARD TC - LOOKS LIKE POSS INJURIES / 1039 HLFD  [Shared]Nov 14 2017  4:29PM [9] ALSO , WB 94 JEO 125 -  PIECES OF A VEH IN FAST LN - ONE VEH FACING W/ WAY  [Shared]Nov 14 2017  4:27PM [5] VEHS MAY NEEDS TOWS, UNK AT THIS TIMENov 14 2017  4:27PM [4] ALL VEHS IN THE CDNov 14 2017  4:27PM [3] NEG 1125Nov 14 2017  4:27PM [2] SIL HOND HYBRED VS 2 MIDSIZE SUVS Nov 14 2017  4:26PM [1] 2-3 VEHS INV Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114IN00948
+    https://www.google.com/maps/search/-117.302924,34.12149
+    Nov 14 2017  4:54PM  1125-Traffic Hazard  I215 S / Baseline St 
+  Nov 14 2017  4:57PM [3] [FSP] has closed their incident []Nov 14 2017  4:54PM [1] [1] OFR - WHI CHEV IMP IN #3 - WILL ADV[Shared] Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114INFSP00231
+    https://www.google.com/maps/search/-117.331191,33.97173
+    Nov 14 2017  4:43PM  1125-Traffic Hazard  I215 N No / Martin Luther King Blvd Mlk 
+  Nov 14 2017  4:43PM [1] BLK HOND PILOT IN REV GP Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114OC00463
+    https://www.google.com/maps/search/-117.643228,33.601688
+    Nov 14 2017  4:56PM  1183-Trfc Collision-Unkn Inj  Arbolitos / La Paz Rd 
+  Nov 14 2017  4:57PM [5] VEH WAS T-BONED Nov 14 2017  4:57PM [4] TRANS OCSONov 14 2017  4:56PM [3] PERSON TRAPPED BUT IS OK - CAN'T GET OO VEHNov 14 2017  4:56PM [2] VEH ON ITS SIDENov 14 2017  4:56PM [1] SOLO VEH ROLLOVER  Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114OCFSP00087
+    https://www.google.com/maps/search/-118.090138,33.780289
+    Nov 14 2017  4:37PM  1125-Traffic Hazard  I405 N / I405 N I605 N Con 
+  Nov 14 2017  4:37PM [4] [4] NEAR MISS TC [Shared]Nov 14 2017  4:36PM [1] [1] ON VERY NARROW RS [Shared] Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114OC00448
+    https://www.google.com/maps/search/-118.090138,33.780289
+    Nov 14 2017  4:36PM  1125-Traffic Hazard  I405 N / I405 N I605 N Con 
+  Nov 14 2017  4:37PM [4] NEAR MISS TC Nov 14 2017  4:36PM [1] ON VERY NARROW RS  Information courtesy of 
+  Last updated: 11/14/2017 4:58pm 
+
+  CHP Incident 171114OC00394
+    https://www.google.com/maps/search/-117.717359,33.859686
+    Nov 14 2017  3:19PM  1182-Trfc Collision-No Inj  Sr241 S / Sr91 E Sr241 S Con 
+  Nov 14 2017  4:06PM [8] [Notification] [CHP]-PER 3A BITETTOS TOW  ETA 1HR OR LESSNov 14 2017  4:04PM [7] [Notification] [CHP]-ON HOLD W/ 3A  -  1039 3A, SVC# 49217, Information courtesy of 
+  Last updated: 11/14/2017 4:58pm
